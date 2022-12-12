a4b17bd09401cd972733ec1266c1f680ec04eb6d
anhhm-john <anhhm.2003.work@gmail.com>
2022-12-13 00:07:06 +0700

    convert to jsx

--- src/App.js
+++ src/App.js
@@ -8,6 +8,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp, faPhone } from '@fortawesome/free-solid-svg-icons';
 import images from './assets/images';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyCsjDAeY41gCWLuVA_ibJ5jjYuDSxp-zyU',
+    authDomain: 'web-order-project.firebaseapp.com',
+    projectId: 'web-order-project',
+    storageBucket: 'web-order-project.appspot.com',
+    messagingSenderId: '335212962455',
+    appId: '1:335212962455:web:130ae5406a0a8cf674bfd2',
+    measurementId: 'G-0CHFG7J5L1',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const cx = classNames.bind(styles);
 
 function App() {
